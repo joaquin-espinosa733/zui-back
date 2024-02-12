1b436a06c795ab3e16336e1d980d3135fda11fb4
joaquin-espinosa733 <juaco.enero9@gmail.com>
2024-02-12 20:42:02 -0300

    modelo user

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,7 @@
+import { User } from "../interface/user.interface"
+import { handleHttp } from "../../utils/error"
+
+
+const registerUser = (user: User) => {
+
+}
---
+++ src/models/user.ts
@@ -0,0 +1,30 @@
+import { Schema, Types, model, Model } from "mongoose";
+import { User } from "../interface/user.interface";
+
+const userSchema = new Schema<User>({
+    userName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    birthdate: {
+        type: String,
+        required: true
+    }
+},
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+);
+
+const UserModel = model("user", userSchema);
+
+export default UserModel;
