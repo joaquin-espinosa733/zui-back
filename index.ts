51f54585427c90d30896f1f146ae95c49842f041
joaquin-espinosa733 <juaco.enero9@gmail.com>
2024-02-12 20:26:31 -0300

    db conectada

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from "express"
+import db from "./src/config/db";
+import cors from "cors"
 const app = express()
 const port = 3000
+
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) =>
     res.send('Hello World!')
 )
 
+
+db().then(() => console.log("Conexion lista"))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
